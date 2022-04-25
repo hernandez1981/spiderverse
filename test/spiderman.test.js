61aa69f628e9e1a06c65fa25a347c11c11b24229
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-25 16:51:36 -0600

    Paso 5:Modificamos la prueba de unidad

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,15 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../app/spiderman');
+describe("Unit Tests for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+
+        //Aqui escribimos el código que queremos usar tal cual
+        //Quiero poder instanciar un objeto Spiderman con esta información
+      const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Galfield", 2, "Sony")
+      expect(andrewGarfield.name).toBe("Spiderman Sony");
+      expect(andrewGarfield.age).toBe(31);
+      expect(andrewGarfield.actor).toBe("Andrew Garfield");
+      expect(andrewGarfield.movies).toBe(31);
+      expect(andrewGarfield.studio).toBe("Sony");
+      
     });
   })
