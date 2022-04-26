b2ab9ae55f51baf42557e38c8e7dedf29376a2bb
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-25 18:15:13 -0600

    Paso 10: Agregamos método getInfo() en la clase spiderman.js

--- app/spiderman.js
+++ app/spiderman.js
@@ -4,9 +4,11 @@ class Spiderman {
         this.age = age
         this.actor = actor
         this.movies = movies
-        this.studio = studio
-        
+        this.studio = studio        
     }
+    getInfo(){ // es una función que ejecutará cualquier objeto instanciado de esta clase
+        return `Hey, I'm ${this.actor} from ${this.studio} studio`
+      }
 }
 
 module.exports = Spiderman
